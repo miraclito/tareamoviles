587948ebd0396b7585a61a2d41ca1568a8535215
hirampro1 <109707082+hirampro1@users.noreply.github.com>
2024-09-23 12:30:17 -0500

    tarea 1

---
+++ app/src/main/java/pe/edu/upeu/asistenciaupeujcn/ui/presentation/screens/asistencia/ListaDeAsistencia.kt
@@ -0,0 +1,9 @@
+package pe.edu.upeu.asistenciaupeujcn.ui.presentation.screens.asistencia
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun listaAsis() {
+    Text(text = "lista de asistencia")
+}
